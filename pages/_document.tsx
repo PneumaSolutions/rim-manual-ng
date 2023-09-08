943ecef72719e9cdbbc9a53c34e5648f69f81860
Matt Campbell <mattcampbell@pobox.com>
2023-09-08 09:59:09 -0500

    Committing what I've got, though the client-side code isn't running

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { PlatformProvider, PlatformSwitcher } from '../components/platform'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        <PlatformProvider>
+          <Main />
+          <PlatformSwitcher />
+        </PlatformProvider>
         <NextScript />
       </body>
     </Html>
