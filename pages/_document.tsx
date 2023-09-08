470bba599e5d23c89be81242f89c646c681365c4
Matt Campbell <mattcampbell@pobox.com>
2023-09-08 12:23:22 -0500

    Get platform selection working client-side

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import { PlatformProvider, PlatformSwitcher } from '../components/platform'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <PlatformProvider>
-          <Main />
-          <PlatformSwitcher />
-        </PlatformProvider>
+        <Main />
         <NextScript />
       </body>
     </Html>
