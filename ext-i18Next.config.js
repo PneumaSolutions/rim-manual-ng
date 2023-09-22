e8e9180e71ca62843c9373e05a046aafd5cee19c
Matt Campbell <mattcampbell@pobox.com>
2023-09-22 08:48:52 -0500

    Fix errors in getStatic.js; remove unnecessary files added while experimenting

--- ext-i18Next.config.js
+++
@@ -1,9 +0,0 @@
-// next-i18next.config.js
-
-module.exports = {
-    debug: process.env.NODE_ENV === 'development',
-    i18n: {
-      locales: ['en', 'es'],
-      defaultLocale: 'en',
-    },
-  };
