45074821f775adb6031aab0998de5a7fd826e4be
jackf723 <jackf723@gmail.com>
2023-10-02 09:56:53 -0500

    title text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,15 +11,79 @@ import  NavList from '@/components/NavList'
 // @ts-ignore  
 import  FooterNav from '@/components/FooterNav'
 import { PlatformProvider, PlatformSwitcher } from '../components/platform'
+import { useRouter } from 'next/router';
+import languageMappings from '@/components/languageMappings'; // translation dictionary
+
 // import { MDXLayout } from '@/components/MDXLayout';
 //function app({ Component, pageProps }: AppProps) {
   export default function App({ Component, pageProps }: AppProps) {
-
+    const router = useRouter();
+    const currentLang = (router.asPath.match(/\/(en|es|fr|it|sv|de|pt)(\/|$)/) || [])[1] || 'en';
+    // Let's define our default path/text sets
+    const {
+      homePath,
+      homeText,
+      homeTitleText,
+      specsPath,
+      specsText,
+      specsTitleText,
+      instPath,
+      instText,
+      instTitleText,
+      gcPath,
+      gcText,
+      gcTitleText,
+      wwsPath,
+      wwsText,
+      wwsTitleText,
+      dashPath,
+      dashText,
+      dashTitleText,
+      faqPath,
+      faqText,
+      faqTitleText,
+      planPath,
+      planText,
+      planTitleText,
+      clPath,
+      clText,
+      clTitleText,
+    } = languageMappings[currentLang];  
+  let titleText="";
+  // Let's define our titles per page.
+  if (router.asPath.match(/^\/(en|es|fr|it|sv|de|pt)\/?$/)) {
+    titleText = homeTitleText;
+  }
+  if (router.asPath.includes('/systemspecs')) {
+    titleText = specsTitleText;
+  }
+  if (router.asPath.includes('/installation')) {
+    titleText = instTitleText;
+  }
+  if (router.asPath.includes('/gettingconnected')) {
+    titleText = gcTitleText;
+  }
+  if (router.asPath.includes('/workingwithsessions')) {
+    titleText = wwsTitleText;
+  }
+  if (router.asPath.includes('/dashboard')) {
+    titleText = dashTitleText;
+  }
+  if (router.asPath.includes('/faq')) {
+    titleText = faqTitleText;
+  }
+  if (router.asPath.includes('/plans')) {
+    titleText = planTitleText;
+  }
+  if (router.asPath.includes('/changelog')) {
+    titleText = clTitleText;
+  }
+  
     return (
 
 <PlatformProvider>
 <Head>
-  <title>{pageProps.mdxTitle || 'Default Title'}</title>
+  <title>{titleText}</title>
 </Head>
 <NavList />
 <SearchDialog />
