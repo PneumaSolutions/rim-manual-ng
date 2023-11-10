8cf0b3f9724604e10015aaddfa34c75dc89205de
Matt Campbell <mattcampbell@pobox.com>
2023-11-10 10:55:06 -0600

    Reimplement the language switcher

--- src/i18n/en/ui.ts
+++ src/i18n/en/ui.ts
@@ -1,6 +1,8 @@
 export default {
   "footer.next": "Next",
   "footer.previous": "Previous",
+  "language.label": "Language",
+  "language.switchLanguage": "Switch language",
   "platform.switchToWindows": "Switch to Windows content",
   "platform.switchToMacOS": "Switch to macOS content",
 }
