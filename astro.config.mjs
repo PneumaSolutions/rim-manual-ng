36c2a9527bba7036920b2412de440279e54a52a9
Matt Campbell <mattcampbell@pobox.com>
2023-11-10 14:40:51 -0600

    Try to get Vercel hybrid SSR deployment working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config"
 
 import mdx from "@astrojs/mdx"
 import preact from "@astrojs/preact"
+import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
   integrations: [preact({ compat: true }), mdx()],
   server: {
     port: 3000,
