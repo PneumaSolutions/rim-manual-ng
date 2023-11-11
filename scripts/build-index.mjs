a05d1b647bdd5398564bc7147a24e30ff144cf6a
Matt Campbell <mattcampbell@pobox.com>
2023-11-11 11:30:23 -0600

    Build search indices for all languages

--- scripts/build-index.mjs
+++ scripts/build-index.mjs
@@ -13,31 +13,37 @@ import path from "path"
 import matter from "gray-matter"
 import removeMd from "remove-markdown"
 
-const filenames = fs.readdirSync(path.join("./src/content/docs/en/"))
-const data = filenames.map((filename) => {
-  try {
-    const markdownWithMeta = fs.readFileSync(
-      "./src/content/docs/en/" + filename
-    )
-    let { data: frontmatter, content } = matter(markdownWithMeta)
-    content = content.trim()
-    while (content.startsWith("import ")) {
-      let i = content.indexOf("\n")
-      content = content.substring(i + 1).trim()
+function buildIndex(lang) {
+  const filenames = fs.readdirSync(path.join(`./src/content/docs/${lang}/`))
+  const data = filenames.map((filename) => {
+    try {
+      const markdownWithMeta = fs.readFileSync(
+        `./src/content/docs/${lang}/${filename}`
+      )
+      let { data: frontmatter, content } = matter(markdownWithMeta)
+      content = content.trim()
+      while (content.startsWith("import ")) {
+        let i = content.indexOf("\n")
+        content = content.substring(i + 1).trim()
+      }
+      const baseFilename = path.basename(filename, ".mdx")
+      return {
+        id: baseFilename,
+        title: frontmatter.title,
+        content: removeMd(content).replace(/\n/g, " "),
+      }
+    } catch (e) {
+      console.error(`Error processing file ${filename}:`, e.message)
+      return null // or a default object
     }
-    const baseFilename = path.basename(filename, ".mdx")
-    return {
-      id: baseFilename,
-      title: frontmatter.title,
-      content: removeMd(content).replace(/\n/g, " "),
-    }
-  } catch (e) {
-    console.error(`Error processing file ${filename}:`, e.message)
-    return null // or a default object
-  }
-})
-// 2. Send the dataset in JSON format
-client
-  .index("rim-manual-en")
-  .addDocuments(JSON.parse(JSON.stringify(data)))
-  .then((res) => console.log(res)) //show the result
+  })
+  // 2. Send the dataset in JSON format
+  client
+    .index(`rim-manual-${lang}`)
+    .addDocuments(JSON.parse(JSON.stringify(data)))
+    .then((res) => console.log(res)) //show the result
+}
+
+for (let lang of ["de", "en", "es", "fr", "it", "pt", "sv"]) {
+  buildIndex(lang)
+}
